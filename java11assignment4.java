69b5c34e7d9dbafa79770f7dcd70ce6bfb7685db
Sushma-Chinthala <115689413+Sushma-Chinthala@users.noreply.github.com>
2022-11-05 22:08:47 +0530

    Add files via upload

---
+++ java11assignment4.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class java11assignment4
+{
+    public static void main(String[] args)
+    {
+     var path="C:\\Users\\suschint\\sushma\\StudentList.txt";
+     try
+     {
+         String data = Files.readString(Path.of(path));
+         System.out.println(data);
+
+     }catch(IOException e)
+     {
+         e.printStackTrace();
+     }
+
+    }
+}
